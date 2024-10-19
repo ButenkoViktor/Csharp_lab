b15c9f317120cca6a6d8413b4b54ddebfd2ecb2a
User <User@DESKTOP-60TVQ80>
2024-10-19 10:08:27 +0200

    Add project files.

---
+++ Lab01/ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Hello, World!");
+string message = "";
+message += "Hello";
+message += ",";
+message += " ";
+message += "World!";
+Console.WriteLine(message);
+Console.WriteLine("Przejście do\nnowej linii.");
+Console.WriteLine("tabulacja\tpozioma. ");
+Console.WriteLine("Tabulacja\npionowa. ");
+Console.WriteLine("Alarm!\a");
+Console.WriteLine("Pojedynczy apostrof: \'");
+Console.WriteLine("Podejczy cudzysłów: \"");
+Console.WriteLine("Znak pusty: \0");
